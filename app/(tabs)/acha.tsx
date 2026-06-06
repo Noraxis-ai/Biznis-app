f5b371b81fbb152b351f6d301413a849751fdfda
Stanley Norcius <norciusstanley@gmail.com>
2026-06-06 17:39:56 -0600

    Create app screen structure

--- app/(tabs)/acha.tsx
+++ app/(tabs)/acha.tsx
@@ -1,12 +1,32 @@
+import { Button, StyleSheet, TextInput } from 'react-native';
+
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 
 export default function AchaScreen() {
   return (
-    <ThemedView>
-      <ThemedText type="title">
-        Paj Acha
-      </ThemedText>
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Acha</ThemedText>
+
+      <TextInput placeholder="Non pwodwi pou acha" style={styles.input} />
+      <TextInput placeholder="Pri acha" style={styles.input} keyboardType="numeric" />
+      <TextInput placeholder="Kantite acha" style={styles.input} keyboardType="numeric" />
+
+      <Button title="Anrejistre Acha" onPress={() => alert('Test Acha')} />
     </ThemedView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+
+  input: {
+    borderWidth: 1,
+    marginTop: 15,
+    padding: 10,
+    backgroundColor: 'white',
+    borderRadius: 8,
+  },
+});
--- app/(tabs)/vant.tsx
+++ app/(tabs)/vant.tsx
@@ -1,12 +1,33 @@
+import { Button, StyleSheet, TextInput } from 'react-native';
+
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 
 export default function VantScreen() {
   return (
-    <ThemedView>
-      <ThemedText type="title">
-        Paj Vant
-      </ThemedText>
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Vant</ThemedText>
+
+      <TextInput placeholder="Non kliyan" style={styles.input} />
+      <TextInput placeholder="Non pwodwi" style={styles.input} />
+      <TextInput placeholder="Pri" style={styles.input} keyboardType="numeric" />
+      <TextInput placeholder="Kantite" style={styles.input} keyboardType="numeric" />
+
+      <Button title="Anrejistre Vant" onPress={() => alert('Test Vant')} />
     </ThemedView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+
+  input: {
+    borderWidth: 1,
+    marginTop: 15,
+    padding: 10,
+    backgroundColor: 'white',
+    borderRadius: 8,
+  },
+});
